562aff2636f1b0f57087d88d0b92b2b32dc66eb5
Deepanshu khorwal <deepanshukhorwal779@gmail.com>
2023-01-10 20:01:59 +0530

    e

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/e-commerce";
-mongoose.connect(url);
+const url =
+  "mongodb+srv://deepanshu:<password>@cluster0.cqz501k.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModfiy: false,
+  })
+  .then(() => {
+    console.log(`connection successfull`);
+  })
+  .catch((err) => {
+    console.log(`no connection`);
+  });
